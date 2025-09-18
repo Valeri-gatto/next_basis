bfcc3594b974ff04a71a319f899332bf39b96d21
Valeria <none@example.com>
2025-09-18 23:50:53 +0400

    five chapter

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter } from 'next/font/google';
+import { Playfair_Display } from 'next/font/google'
+import { Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const playfair = Playfair_Display({ subsets: ['latin'], weight: ["400", "800"], preload: true });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ["400", "700"] });
