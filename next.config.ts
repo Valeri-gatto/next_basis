1771256a93b820b5b4cf62e484406203abf09887
Valeria <none@example.com>
2025-09-24 01:04:22 +0400

    chapter fourteen

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
     ppr: 'incremental'
-  }
+  },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // }
 };
+
 
 export default nextConfig;
